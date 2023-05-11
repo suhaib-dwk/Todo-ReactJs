032b7752ffcf36f775d2a283672fd55957747b53
suhaib-dwk <suhaibdwweekat1@gmail.com>
2023-05-11 19:12:47 +0300

    Add Redux

---
+++ src/components/Todos.jsx
@@ -0,0 +1,78 @@
+import { useRef, useState } from "react"
+import { connect } from "react-redux";
+import { addTodos  ,removeTodos, updateTodos } from "../redux/reducer";
+
+const mapStateToProps = (state) => {
+    return {
+        todo: state,
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        addTodo: (obj) => dispatch(addTodos(obj)),
+        removeTodo : (id) => dispatch(removeTodos(id)),
+        updateTodo: (obj) => dispatch(updateTodos(obj)),
+    }
+};
+
+
+const Todos = (props) => {
+
+    const [todo, setTodo] = useState("");
+
+    const inputRef = useRef(true);
+
+    const changeFocus = () => {
+        inputRef.current.disabled = false;
+        inputRef.current.focus();
+    }
+    
+    const update = (id , value , e )  => {
+        if(e.which === 13) {
+            // here 13 is key code for enter key
+            props.updateTodo({id, item : value});
+            inputRef.current.disabled = true;
+        }
+    }
+
+    const handleChange = (e) => {
+        setTodo(e.target.value);
+    }
+
+    console.log( props)
+
+  return (
+    <div className="addTodos">
+        <input type="text"  onChange={(e) => handleChange(e)} className="todo-input" />
+        <button className="add-btn" onClick={() => props.addTodo({
+            id: Math.floor(Math.random() * 1000),
+            item: todo,
+            completed: false,
+        })}>Add</button>
+
+        <br />
+
+        <ul>
+            {
+                props.todo.map(item => {
+                    return(
+                        <li key={item.id}>
+                            <textarea 
+                            ref={inputRef} 
+                            disabled={inputRef} 
+                            defaultValue={item.item}
+                            onKeyPress={(e) => update(item.id , inputRef.current.value, e)}
+                            />
+                            <button onClick={() => changeFocus()}>Edit</button>
+                            <button onClick={() => props.removeTodo(item.id)}>Delete</button>{" "}
+                        </li>
+                    );
+                })
+            }
+        </ul>
+    </div>
+  )
+}
+// use connect method to connect this components with redux store
+export default connect(mapStateToProps,mapDispatchToProps)(Todos);
